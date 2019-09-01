9e7e4cf6f05e920f227643816b00ebe18156e2b8
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-09-01 16:45:36 +0100

    swapped post to get for route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :students, only: [:index, :show]
-  post 'students/:id/activate_student', to: 'students#activate_student'
+  get 'students/:id/activate_student', to: 'students#activate_student'
   
 end
